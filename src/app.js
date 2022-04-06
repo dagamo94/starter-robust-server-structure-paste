ecb455d195a5ff545bc1acedc9a047709f846178
dan <da.ga.mo.94@gmail.com>
2022-04-06 11:31:04 -0600

    add basic error handling functions and adjust error handlers

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ app.use(express.json());
 // TODO: Follow instructions in the checkpoint to implement ths API.
 const pastes = require("./data/pastes-data");
 
-// RETURN ONLY ONE PASTE OBJECT FOUND BY ID
+// *** RETURN ONLY ONE PASTE OBJECT FOUND BY ID ***
 app.get("/pastes/:pasteId", (req, res, next) => {
   const { pasteId } = req.params;
   const foundPaste = pastes.find((paste) => Number(pasteId) === paste.id);
@@ -16,25 +16,41 @@ app.get("/pastes/:pasteId", (req, res, next) => {
   if (foundPaste) {
     res.json({ data: foundPaste });
   } else {
-    next(`Paste id not found: ${pasteId}`);
+    next({
+      status: 404,
+      message: `Paste id not found: ${pasteId}`
+    });
   }
 });
 
-// RETURN/GET ALL PASTES OBJECTS
+// *** RETURN/GET ALL PASTES OBJECTS ***
 app.get("/pastes", (req, res) => {
   res.json({ data: pastes });
 });
 
-// POST HANDLER FOR '/pastes'
+// *** MIDDLEWARE FUNCTION TO VALIDATE THE REQUEST BODY
+function bodyHasTextProperty(req, res, next) {
+  const { data: { text } = {} } = req.body;
+  if (text) {
+    return next(); // Call 'next()' without an error message if the result(text) exists
+  }
+  next({
+    status: 400,
+    message: "A 'text' property is required."
+  });
+}
+
+// *** POST HANDLER FOR '/pastes' ***
 // variable to hold the next ID
 // because some IDs may already be used, find the largest assigned ID
 let lastPasteId = pastes.reduce((maxId, paste) => Math.max(maxId, paste.id), 0);
-app.post("/pastes", (req, res, next) => {
-  const { data: { name, syntax, exposure, expiration, text, user_id } = {} } = req.body; // If the body doesn't contain a data property, th destructuring will still succeed because a defualt value of {} has still been suppied for the 'data' property
-  if(text){
-
+app.post(
+  "/pastes",
+  bodyHasTextProperty,
+  (req, res, next) => {
+    const { data: { name, syntax, exposure, expiration, text, user_id } = {} } = req.body; // If the body doesn't contain a data property, th destructuring will still succeed because a defualt value of {} has still been suppied for the 'data' property
     const newPaste = {
-      id: ++lastPasteId, // Increment last ID, then asign as the current ID
+      id: ++lastPasteId, // Increment last ID, then assign as the current ID
       name,
       syntax,
       exposure,
@@ -44,20 +60,19 @@ app.post("/pastes", (req, res, next) => {
     };
     pastes.push(newPaste);
     res.status(201).json({ data: newPaste });
-  }else{
-    res.sendStatus(400);
   }
+);
+
+// *** Not found handler ***
+app.use((req, res, next) => {
+  next(`Not found: ${req.originalUrl}`);
 });
 
-// Not found handler
-app.use((request, response, next) => {
-  next(`Not found: ${request.originalUrl}`);
-});
-
-// Error handler
-app.use((error, request, response, next) => {
+// *** Error handler ***
+app.use((error, req, res, next) => {
   console.error(error);
-  response.send(error);
+  const { status = 500, message = "Something went wrong. Internal server error." } = error;
+  res.status(status).json({ error: message });
 });
 
 module.exports = app;
